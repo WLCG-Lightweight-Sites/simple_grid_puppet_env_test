c00c6e4c050d987d965b0457ce406985f1a7002d
ejr004 <acessoej@icloud.com>
2018-11-09 14:19:30 +0100

    Adding references for install environment

--- site/profile/manifests/config_master.pp
+++ site/profile/manifests/config_master.pp
@@ -1,3 +1,3 @@
 class profile::config_master {
-  include simple_grid::pre_config::config_master::init
+  include simple_grid::install::config_master::init
 }
--- site/profile/manifests/lightweight_component.pp
+++ site/profile/manifests/lightweight_component.pp
@@ -1,4 +1,4 @@
 class profile::lightweight_component (
 ){
-  include simple_grid::pre_config::lightweight_component::init
+  include simple_grid::install::lightweight_component::init
 }
